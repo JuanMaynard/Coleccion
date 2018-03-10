a95648f0f740bd4cb3453b20f8c03d2d2f6425de
JuanMaynard <martinmaynard56@gmail.com>
2018-03-10 08:34:51 -0300

    creando projecto

---
+++ Coleccion/src/coleccion/Playlist.java
@@ -0,0 +1,26 @@
+package coleccion;
+
+public class Playlist extends Coleccion {
+
+    private Coleccion[] coleccion;
+
+    public Playlist(String nombre) {
+        super(nombre);
+    }
+
+    @Override
+    public int getDuracion() {
+
+    }
+
+    @Override
+    public int getCantidad() {
+
+    }
+
+    @Override
+    public String toString() {
+        return 
+    }
+
+}
